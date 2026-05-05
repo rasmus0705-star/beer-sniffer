776121be3bcc00b5bc93643556a99342e91cccea
Rasmus <rasmus0705@gmail.com>
2026-05-05 17:55:19 +0300

    add CORS

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import beers, scrape
 from app.services.scheduler import scheduler
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(beers.router)
 app.include_router(scrape.router)
-
 
 @app.on_event("startup")
 def start_scheduler():
